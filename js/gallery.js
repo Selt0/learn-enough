c909122d81b2487123655933f9433a21fc4980fb
Selt0 <chewedgum12@gmail.com>
2019-12-20 15:05:54 -0600

    Gallery images are now responsive on click

--- js/gallery.js
+++ js/gallery.js
@@ -1,5 +1,17 @@
-function activateGallery() {
-  alert("Hello from the gallery!");
-}
+  //variable to hold all the images nested inside the div with id: gallery-thumbs
+  let thumbnails = document.querySelectorAll('#gallery-thumbs > div > img');
 
-activateGallery();
+  //set variable to hold img nested inside id: gallery-photo 
+  let mainImage = document.querySelector('#gallery-photo img');
+
+  thumbnails.forEach(function(thumbnail) {
+    thumbnail.addEventListener('click', function() {
+      //set variable to hold path src of file
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      //variable for alt
+      let newImageAlt = thumbnail.dataset.description;
+      //set the src attribute to the current display image to the large version when clicked
+      mainImage.setAttribute('src', newImageSrc);
+      mainImage.setAttribute('alt', newImageAlt);
+    });
+  });
